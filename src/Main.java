5218d79d014ddde25e58c6d8459a207d56abe430
Jos <jlnajera.2002@gmail.com>
2023-02-20 16:16:09 -0600

    Agregando al main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        ProcesosBasicos proc = new ProcesosBasicos();
+
+        proc.pedirDatos();
+        proc.hechosConocidos();
+        proc.mostrarDatos();
     }
 }
---
+++ src/ProcesosBasicos.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+    public class ProcesosBasicos {
+
+        Scanner scan = new Scanner(System.in);
+        String texto;
+        String texto2;
+        int i;
+        int a;
+        int noReglas;
+        int noHechos;
+        public ArrayList<String> reglas = new ArrayList<String>();
+        public ArrayList<String> hechosCon = new ArrayList<String>();
+        public void pedirDatos(){
+            System.out.println("¿Cuántas reglas son?");
+            noReglas=scan.nextInt();
+            scan.nextLine();
+            System.out.println("Recuerda ingresar las reglas del siguiente modo (R1 = si h2 y h3 -> h6");
+            System.out.println("\n");
+            for(i=0;i<noReglas;i++){
+                System.out.println("Dame R"+(i+1)+": ");
+                texto = scan.nextLine();
+                reglas.add(texto);
+                scan.nextLine();
+            }
+        }
+        public void mostrarDatos(){
+            for (a=0;a<reglas.size();a++){
+                System.out.println(reglas.get(a));
+            }
+            System.out.println("Base de conocimiento {");
+            for (a=0;a<hechosCon.size();a++){
+                System.out.println(hechosCon.get(a));
+            }
+            System.out.println("}");
+        }
+
+        public void hechosConocidos(){
+            System.out.println("¿Cuántos elementos hay en la base de conocimiento?");
+            noHechos=scan.nextInt();
+            scan.nextLine();
+
+            for (i=0;i<noHechos;i++) {
+                System.out.println("Dame los hechos conocidos");
+                texto2 = scan.nextLine();
+                hechosCon.add(texto2);
+                scan.nextLine();
+            }
+        }
+    }
+
